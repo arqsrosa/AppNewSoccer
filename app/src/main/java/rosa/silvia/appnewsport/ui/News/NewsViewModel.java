5a60e854442777b7a880c65abb82b211193c8e7d
Silvia Rosa Domingues <arqsrosa@gmail.com>
2022-07-11 17:28:22 -0300

    atualizando

--- app/src/main/java/rosa/silvia/appnewsport/ui/News/NewsViewModel.java
+++ app/src/main/java/rosa/silvia/appnewsport/ui/News/NewsViewModel.java
@@ -4,16 +4,20 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.List;
+
+import rosa.silvia.appnewsport.domain.News;
+
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<List<News>> news;
 
     public NewsViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is news fragment");
     }
 
-    public LiveData<String> getText() {
+    public LiveData<List<News>> getText() {
         return mText;
     }
 }
